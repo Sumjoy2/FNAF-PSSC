9ad9b2ccdd96cec31aa77749a9e4e198a7b6f27c
Sumjoy2 <5011286mountjoy@gmail.com>
2023-12-12 13:01:48 -0800

    GenearatorWorking

--- Assets/Scrip/3d/Generator.cs
+++ Assets/Scrip/3d/Generator.cs
@@ -1,29 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Properties;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class Generator : MonoBehaviour
 {
-    public float generatorAmount = 500;
+    private int generatorAmount;
+    public int generatorMax = 1000;
     public bool refueling = false;
+    public Life genAmountSlider;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        generatorAmount = generatorMax;
+        genAmountSlider.SetMaxHealth(generatorMax);
+        genAmountSlider.SetHealth(generatorAmount);
     }
 
     private void FixedUpdate()
     {
+        //subtracts from generator amount every fixed update
         generatorAmount--;
-        //if (refueling)
+        genAmountSlider.SetHealth(generatorAmount);
+        //when button held add 5 to generator amount per fixed update
+        if (refueling )
+        {
+            WindUp();
+        }
+        //caps generator amount at generator max
+        if (generatorAmount >= generatorMax)
+        {
+            generatorAmount = generatorMax;
+        }
 
     }
-
+    //adds 5 per whatever to generator amount
     public void WindUp()
     {
         generatorAmount += 5;
+    }
+    //makes holding the button work
+    public void isRefuling()
+    {
+        refueling = !refueling;
     }
 }
