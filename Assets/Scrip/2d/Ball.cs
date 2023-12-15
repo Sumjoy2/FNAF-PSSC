28ae87359859253e0a0dc59dc8d1539dd50cb61e
xXSalGodXx <salgadojj9817@gmail.com>
2023-12-15 13:18:01 -0800

    ok

--- Assets/Scrip/2d/Ball.cs
+++ Assets/Scrip/2d/Ball.cs
@@ -24,7 +24,7 @@ public class Ball : MonoBehaviour
         //Sets initial movement down and with a random X
         rigidbody2d.AddForce(new Vector2(Random.Range(-initialX,initialX), -speed)); 
 
-        //Gets health scipt, Sets max health to healthMax, Sets current health to max health
+        //Gets health script, Sets max health to healthMax, Sets current health to max health
         health = GameObject.FindGameObjectWithTag("HealthBar").GetComponent<Life>();
         health.SetMaxHealth(healthMax);
         healthCurrent = healthMax;
@@ -42,6 +42,11 @@ public class Ball : MonoBehaviour
 
         //When life hits 0 restart level
         if (healthCurrent <= 0) Reset();
+
+        if (gameObject.tag == "Brick")
+        {
+            LoadScene("ActualGame");
+        }
     }
 
     //Checks if something collides with the ball
@@ -69,5 +74,10 @@ public class Ball : MonoBehaviour
     private void Reset()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
     }
 }
