6378de096ef26d5c1848c665f33dcf1dc8ed80bc
Sumjoy2 <5011286mountjoy@gmail.com>
2023-12-15 13:21:03 -0800

    NavMeshKindaWorking

--- Assets/Scrip/3d/enmy moving.cs
+++ Assets/Scrip/3d/enmy moving.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.AI;
+using UnityEngine.AI;
 
 public class enmymoving : MonoBehaviour
 {
@@ -9,37 +11,41 @@ public class enmymoving : MonoBehaviour
     public int night;
     int rand;
     public float timer;
+    private NavMeshAgent navMeshAgent;
+    public Transform targetMove;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
+        navMeshAgent = GetComponent<NavMeshAgent>();
         StartCoroutine(Roll(timer - night));
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (rand > level)
+        navMeshAgent.destination = targetMove.position;
+
+        if (rand <= level)
         {
-            return;
-        }
-        else if (rand <= level)
-        {
-            MoveForward();
+            StartCoroutine(MoveForward());
             rand = 21;
-            
         }
     }
 
-    public void MoveForward()
+    IEnumerator MoveForward()
     {
         //some code here
-        transform.position += new Vector3 (rand, 0 ,0);
+        navMeshAgent.destination = targetMove.position;
+        navMeshAgent.isStopped = false;
+        yield return new WaitForSeconds(3);
         StartCoroutine(Roll(timer - night));
     }
 
     IEnumerator Roll(float time)
     {
+        navMeshAgent.isStopped = true;
+        navMeshAgent.ResetPath();
         yield return new WaitForSeconds(time);
         rand = Random.Range(0, levelMax);
     }
