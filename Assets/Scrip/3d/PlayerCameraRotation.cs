f56bdaab905b634f16a8ccd363833ca35b9e4f05
Sumjoy2 <5011286mountjoy@gmail.com>
2024-01-09 12:10:18 -0800

    Camera Rotation

---
+++ Assets/Scrip/3d/PlayerCameraRotation.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraRotation : MonoBehaviour
+{
+    public float speed = 2.0f;
+    public float angle;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        angle += speed * Input.GetAxis("Mouse X");
+        transform.eulerAngles = new Vector3(0, angle, 0);
+    }
+}
