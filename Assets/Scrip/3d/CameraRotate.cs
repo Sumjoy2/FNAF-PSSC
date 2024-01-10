b9fb38d8708e1ac5850042a81b77e048c120bb15
Blake Schutiz <Blakeduane1120@gmail.com>
2024-01-10 13:18:12 -0800

    asd

--- Assets/Scrip/3d/CameraRotate.cs
+++ Assets/Scrip/3d/CameraRotate.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,10 +9,14 @@ public class CameraRotate : MonoBehaviour
     public float speed;
     Vector3 A;
     Vector3 B;
+
+    public GameObject[] locals;
+    int arrayCurrentLocal = 0;
     void Start()
     {
         A = transform.eulerAngles + new Vector3(0, currentRotation, 0);
         B = transform.eulerAngles + new Vector3(0, -currentRotation, 0);
+        locals = GameObject.FindGameObjectsWithTag("CamearLocals");
     }
 
     // Update is called once per frame
@@ -19,5 +24,17 @@ public class CameraRotate : MonoBehaviour
     {
         float timeering = Mathf.PingPong(Time.time * speed, 1);
         transform.eulerAngles = Vector3.Lerp(A, B, timeering);
+    }
+
+    public void nextCamera()
+    {
+        transform.position = locals[arrayCurrentLocal].transform.position;
+        arrayCurrentLocal++;
+    }
+
+    public void prevCamera()
+    {
+        transform.position = locals[arrayCurrentLocal].transform.position;
+        arrayCurrentLocal--;
     }
 }
--- Assets/Scrip/3d/NIghtNightTimer.cs
+++ Assets/Scrip/3d/NIghtNightTimer.cs
@@ -3,29 +3,47 @@ using System.Collections;
 using TMPro;
 using UnityEngine;
 
+
+
 public class NIghtNightTimer : MonoBehaviour
 {
+  
     [SerializeField] private int startingTime;
     [SerializeField] private float timeUntilHourChange;
     [SerializeField] private TMP_Text timeText;
     [SerializeField] private int timeLimit;
+    [SerializeField] private TMP_Text txt_night;
+
     int day = 1;
+
 
     [NonSerialized] public int timeHours;
 
+
+    public GameObject night;
+
     private void Start()
     {
+        night.SetActive(false);
         timeHours = startingTime;
         StartCoroutine(advanceHourOverTime());
-    }
 
+    }
     private void Update()
     {
         timeText.text = timeHours + ":00 AM";
-        if (timeHours == 6)
+        if (timeHours == timeLimit)
         {
-            day++;
+            night.SetActive(true);
         }
+    }
+    //used to click button to go to next day
+    public void newDay()
+    {
+        day++;
+      
+       night.SetActive(false);
+       
     }
 
     private IEnumerator advanceHourOverTime()
