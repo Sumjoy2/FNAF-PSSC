d168b3fee17c43de3ef59678a20c3db4f09b970e
Sumjoy2 <5011286mountjoy@gmail.com>
2024-01-19 19:49:01 -0800

    ForNotSureFix

--- Assets/Scrip/Materls.cs
+++ Assets/Scrip/Materls.cs
@@ -1,9 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class LightFlicker : MonoBehaviour
+public class Materls : MonoBehaviour
 {
     public float flickerInrensity = 0.2f;
     public float flickerspersecond = 3.0f;
@@ -11,20 +10,18 @@ public class LightFlicker : MonoBehaviour
 
     private float time;
     private float startingIntensity;
-    private Light lights;
+    private Material material;
 
     // Start is called before the first frame update
     void Start()
     {
-        startingIntensity = lights.intensity;
-        if (lights == null) return;
-        lights = GetComponent<Light>();
+        material = GetComponent<Material>();
     }
 
     // Update is called once per frame
     void Update()
     {
         time += Time.deltaTime * (1 - Random.Range(-speedRandomness, speedRandomness)) * Mathf.PI;
-        lights.intensity = startingIntensity + Mathf.Sin(time * flickerspersecond) * flickerspersecond;
+        material.SetColor("_EmissiveColor", Color.white * (startingIntensity + Mathf.Sin(time * flickerspersecond)) * flickerspersecond);
     }
 }
